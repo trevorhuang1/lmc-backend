f5705ece1471543af3a3dcd58d6d0b1a862e3e55
hanlunli <hanlunli.007@gmail.com>
2024-02-28 21:30:22 -0800

    a

--- model/users.py
+++ model/users.py
@@ -87,7 +87,7 @@ class User(db.Model):
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, items='', password="123qwerty", dob=date.today(), favoritefood='guac', role="User", points = 0, friends='', friendrq=''):
+    def __init__(self, name, uid, items="[]", password="123qwerty", dob=date.today(), favoritefood='guac', role="User", points = 0, friends='', friendrq=''):
         self._name = name    # variables with self prefix become part of the object, 
         self._uid = uid
         self._friends = friends
